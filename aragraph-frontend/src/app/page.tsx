deb6c822ebc78114bedaeeaa408e85d39966343f
lihviaa <liviaalabarse@gmail.com>
2024-11-10 15:51:06 -0300

    Adicionando background na home

--- aragraph-frontend/src/app/page.tsx
+++ aragraph-frontend/src/app/page.tsx
@@ -2,8 +2,10 @@ import BirdList from "@/components/BirdList";
  
 export default function Home() { 
   return ( 
+    <main className="flex flex-col items-center font-sans bg-gray-100 min-h-screen bg-[url('/images/equipe/background_equipe.jpg')] bg-cover bg-center">
     <div className="container flex  mx-auto px-4 py-8"> 
       <BirdList /> 
     </div>
+    </main>
   ); 
 }
